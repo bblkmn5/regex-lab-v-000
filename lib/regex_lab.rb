6420e187c743ca67fe989438c6a66bbb92209db3
Ben Blackman <bblkmn5@gmail.com>
2017-10-08 17:55:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,12 +10,12 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
-#[+-.,!?@#$%^&*();\/|<>"'] is \W
+# \W is the same as [+-.,!?@#$%^&*();\/|<>"']
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].+\W$/) ? true: false
 end
 
-#\d is [0-9], * is going through a line zero or more times (repetition quantifier)
+#\d is the same as [0-9], * is going through a line zero or more times (repetition quantifier)
 def valid_phone_number?(phone)
   phone.match(/(\d *){10}|(\d{3}-\d{4})|(\d{7})/) ? true: false
 end
